73c0b0718d5b79cef072a6f0fbc965c7c695bb1a
Maksym Tarasenko <51783745+tarasen-mv@users.noreply.github.com>
2020-09-07 15:29:44 +0300

    Add esential files and folders

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildManager : MonoBehaviour
+{
+
+	public bool building;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter () {
+
+    	transform.GetChild(0).GetComponent<Image>().color = !building ? Color.green : Color.red;
+    	
+
+    }
+
+    void OnMouseExit () {
+
+    	transform.GetChild(0).GetComponent<Image>().color = Color.white;
+
+    }
+}
